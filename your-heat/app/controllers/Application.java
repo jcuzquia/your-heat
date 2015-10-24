f57b3564e83b06ee321d99010bf4d15fa6af86d8
Camilo Uzquiano <camilo@solarenergy.org>
2015-10-24 17:11:13 -0500

    failed to plug the play authenticate

--- your-heat/app/controllers/Application.java
+++ your-heat/app/controllers/Application.java
@@ -1,14 +1,52 @@
 package controllers;
 
-import play.*;
-import play.mvc.*;
+import com.feth.play.module.pa.PlayAuthenticate;
+import com.feth.play.module.pa.user.AuthUser;
 
-import views.html.*;
+import be.objectify.deadbolt.java.actions.Group;
+import be.objectify.deadbolt.java.actions.Restrict;
+import models.User;
+import play.Routes;
+import play.mvc.Controller;
+import play.mvc.Http.Session;
+import play.mvc.Result;
+import views.html.index;
+import views.html.profile;
+import controllers.Signup;
+
 
 public class Application extends Controller {
 
+	public static final String USER_ROLE = "user";
+	public static final String FLASH_ERROR_KEY = "error";
+	public static final String FLASH_MESSAGE_KEY = "message";
+	
     public Result index() {
-        return ok(index.render("Your new application is ready."));
+        return ok(index.render());
     }
+    
+    
+    public static Result jsRoutes() {
+		
+    return ok(Routes.javascriptRouter("jsRoutes", controllers.routes.javascript));	
+//    	return ok(
+//				Routes.javascriptRouter("jsRoutes",
+//						controllers.routes.javascript.Signup.forgotPassword()))
+//				.as("text/javascript");
+	}
+    
+    @Restrict(@Group(Application.USER_ROLE))
+    public static Result restricted(){
+    	final User localUser = getLocalUser(session());
+    	return ok(profile.render(localUser));
+    }
+
+
+	public static User getLocalUser(Session session) {
+		
+		final AuthUser currentAuthUser = PlayAuthenticate.getUser(session);
+		final User localUser = User.findByAuthUserIdentity(currentAuthUser);
+		return null;
+	}
 
 }
--- your-heat/build.sbt
+++ your-heat/build.sbt
@@ -7,15 +7,21 @@ lazy val root = (project in file(".")).enablePlugins(PlayJava, PlayEbean)
 scalaVersion := "2.11.6"
 
 libraryDependencies ++= Seq(
+  "be.objectify"  %% "deadbolt-java"     % "2.4.0",
+  "com.feth"      %% "play-authenticate" % "0.7.0-SNAPSHOT",
+  "org.postgresql"    %  "postgresql"        % "9.4-1201-jdbc41",
   javaCore,
   cache,
   jdbc,
   ws,
   evolutions,
-  "org.webjars" %% "webjars-play" % "2.4.0-1",
-  "org.webjars" % "bootstrap" % "3.3.4",
+  "org.webjars" % "bootstrap" % "3.2.0",
+  "org.easytesting" % "fest-assert" % "1.4" % "test",
   filters
 )
+
+//add resolver for deadbolt and easymail snapshots
+resolvers += Resolver.sonatypeRepo("snapshots")
 
 // Play provides two styles of routers, one expects its actions to be injected, the
 // other, legacy style, accesses its actions statically.
